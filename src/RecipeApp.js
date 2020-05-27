bed1d1a501e2f3712f7aca6e412a4178c95f5e96
anish391 <argads@gmail.com>
2020-05-27 09:23:42 +0000

    Send state up to RecipeApp.

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Recipe from './Recipe';
 import RecipeList from './RecipeList';
 import Navbar from './Navbar';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
-  render() {
-    return (
-      <div className="App">
-		<Navbar />
-        <RecipeList />
-      </div>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			recipes: [
+				{
+					id: 1,
+					title: "Spaghetti",
+					instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
+					ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+					img: "spaghetti.jpg"
+				},
+				{
+					id: 2,
+					title: "Milkshake",
+					instructions: "Combine ice cream and milk.  Blend until creamy",
+					ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
+					img: "milkshake.jpg"
+				},
+				{
+					id: 3,
+					title: "Avocado Toast",
+					instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+					ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
+					img: "avocado_toast.jpg"
+				}
+			],
+			nextRecipeId: 4,
+		}
+	}
+	
+	render() {
+		return (
+			<div className="App">
+				<Navbar />
+				<RecipeList recipes={this.state.recipes}/>
+			</div>
+		);
+	}
 }
 
 export default RecipeApp;
